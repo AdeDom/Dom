ca6e8dd21b15e30f924fb40192ec9f98dd1358d7
AdeDom <pathiphonyw@gmail.com>
2020-01-15 15:44:34 +0700

    init commit

---
+++ app/src/main/java/com/adedom/utility/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.adedom.utility
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
