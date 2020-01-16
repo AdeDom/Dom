afca5fdd6a2ef2adc4885483c1d7184222c4f3bc
AdeDom <pathiphonyw@gmail.com>
2020-01-17 01:44:53 +0700

    set & remove Marker

--- library/src/main/java/com/adedom/library/util/Utility.kt
+++ library/src/main/java/com/adedom/library/util/Utility.kt
@@ -1,8 +1,84 @@
 package com.adedom.library.util
 
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.*
 import java.text.SimpleDateFormat
 import java.util.*
 
+//getDateTime(KEY_DATE)
 fun getDateTime(keyDateTime: String): String =
     SimpleDateFormat(keyDateTime, Locale.ENGLISH).format(Calendar.getInstance().time)
 
+var myLocation: Marker? = null
+var myCircle: Circle? = null
+//setMarker(
+//    MapActivity.sGoogleMap!!,
+//    latLng,
+//    BitmapDescriptorFactory.fromResource(R.drawable.ic_player),
+//    player.name!!,
+//    getLevel(player.level)
+//)
+fun setMarker(
+    googleMap: GoogleMap?,
+    latLng: LatLng,
+    icon: BitmapDescriptor,
+    title: String? = null,
+    snippet: String? = null
+) {
+    myLocation = googleMap!!.addMarker(
+        MarkerOptions()
+            .position(latLng)
+            .icon(icon)
+            .title(title)
+            .snippet(snippet)
+    )
+}
+
+//setMarkerList(
+//    markerPlayers,
+//    MapActivity.sGoogleMap!!,
+//    latLng,
+//    BitmapDescriptorFactory.fromBitmap(it),
+//    item.name,
+//    getLevel(item.level)
+//)
+fun setMarkerList(
+    markers: ArrayList<Marker>,
+    googleMap: GoogleMap?,
+    latLng: LatLng,
+    icon: BitmapDescriptor,
+    title: String? = null,
+    snippet: String? = null
+) {
+    markers.add(
+        googleMap!!.addMarker(
+            MarkerOptions().position(latLng)
+                .icon(icon)
+                .title(title)
+                .snippet(snippet)
+        )
+    )
+}
+
+//setCircle(MapActivity.sGoogleMap, latLng, RADIUS_ONE_HUNDRED_METER)
+fun setCircle(googleMap: GoogleMap?, latLng: LatLng, radius: Double) {
+    myCircle = googleMap!!.addCircle(
+        CircleOptions().center(latLng)
+            .radius(radius)
+            .fillColor(android.R.color.white)
+            .strokeColor(android.R.color.white)
+            .strokeWidth(5f)
+    )
+}
+
+//removeMarker(myLocation)
+fun removeMarker(marker: Marker?) = marker?.remove()
+
+//removeMarkerList(markerPlayers)
+fun removeMarkerList(markers: ArrayList<Marker>) {
+    for (marker in markers) marker.remove()
+    markers.clear()
+}
+
+//removeCircle(myCircle)
+fun removeCircle(circle: Circle?) = circle?.remove()
