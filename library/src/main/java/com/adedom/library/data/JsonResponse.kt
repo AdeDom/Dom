7855dedf986b2270cf4815ee02caed778af02b9a
AdeDom <pathiphonyw@gmail.com>
2020-01-18 22:13:39 +0700

    Json

--- library/src/main/java/com/adedom/library/data/JsonResponse.kt
+++ library/src/main/java/com/adedom/library/data/JsonResponse.kt
@@ -1,5 +1,6 @@
 package com.adedom.library.data
 
+import com.adedom.library.util.KEY_RESULT
 import com.google.gson.annotations.SerializedName
 
 data class JsonResponse(@SerializedName(KEY_RESULT) val result: String? = null)
