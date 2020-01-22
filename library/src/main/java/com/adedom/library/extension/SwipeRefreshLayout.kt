fed2144b2018000e6bb21f11d41274c458a80556
AdeDom <pathiphonyw@gmail.com>
2020-01-23 00:13:01 +0700

    SwipeRefreshLayout

---
+++ library/src/main/java/com/adedom/library/extension/SwipeRefreshLayout.kt
@@ -0,0 +1,15 @@
+package com.adedom.library.extension
+
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+
+fun SwipeRefreshLayout.setSwipeRefresh(refresh: () -> Unit) {
+    this.apply {
+        setColorSchemeResources(
+            android.R.color.holo_red_light,
+            android.R.color.holo_orange_light,
+            android.R.color.holo_green_light,
+            android.R.color.holo_blue_light
+        )
+        setOnRefreshListener { refresh.invoke() }
+    }
+}
