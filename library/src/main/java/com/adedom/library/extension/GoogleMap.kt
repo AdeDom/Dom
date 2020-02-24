0bde5ace5ef33a5461ff39cfba73ab7f4f2850ec
AdeDom <pathiphonyw@gmail.com>
2020-02-24 23:45:51 +0700

    Marker & Circle

--- library/src/main/java/com/adedom/library/extension/GoogleMap.kt
+++ library/src/main/java/com/adedom/library/extension/GoogleMap.kt
@@ -3,40 +3,29 @@ package com.adedom.library.extension
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.*
 
-var myMarker: Marker? = null
-var myCircle: Circle? = null
-//GoogleMapActivity.sGoogleMap!!.setMarker(
-//    latLng,
-//    BitmapDescriptorFactory.fromResource(R.drawable.ic_player),
-//    player.name!!,
-//    getLevel(player.level)
-//)
-fun GoogleMap.setMarker(
+fun GoogleMap.addMarkerOptions(
     latLng: LatLng,
     icon: BitmapDescriptor,
     title: String? = null,
     snippet: String? = null
-) {
-    val marker = this.addMarker(
+): Marker {
+    return this.addMarker(
         MarkerOptions()
             .position(latLng)
             .icon(icon)
             .title(title)
             .snippet(snippet)
     )
-    myMarker = marker
 }
 
-//GoogleMapActivity.sGoogleMap!!.setCircle( latLng, CIRCLE_ONE_HUNDRED_METER)
-fun GoogleMap.setCircle(latLng: LatLng, radius: Double) {
-    val circle = this.addCircle(
+fun GoogleMap.addCircleOptions(latLng: LatLng, radius: Double): Circle {
+    return this.addCircle(
         CircleOptions().center(latLng)
             .radius(radius)
             .fillColor(com.adedom.library.R.color.colorWhite)
             .strokeColor(android.R.color.white)
             .strokeWidth(5f)
     )
-    myCircle = circle
 }
 
 //sGoogleMap!!.setMarkerConstant(druBkk, druIcon, DRU_TITLE, DRU_SNIPPET)
